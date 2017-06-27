5a7eb576d8790e003bed614f03fdaf750341238d
Jinglin Feng <jennyklosever@gmail.com>
2017-06-26 22:19:10 -0400

    Add files via upload

--- Test Updated/ui.R
+++ Test Updated/ui.R
@@ -8,7 +8,7 @@ fluidPage(theme = shinytheme("flatly"),
           tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: #1C2C5B}")),
           fluidRow(
             column(3, offset = 0.5,wellPanel(
-              sliderInput("n", "Number of Samples:", min = 10, max = 1000, value = 50 ,
+              sliderInput("n", "Number of Samples:", min = 20, max = 1000, value = 50 ,
                           step = 1),
               sliderInput("n2", "The number of Categories:", min = 2, max = 8, value = 5 ,
                           step = 1) ,
@@ -26,7 +26,7 @@ fluidPage(theme = shinytheme("flatly"),
             column(6,height=500,plotOutput("plot1", width = 570, height = 430,click = "plot_click")),
            
             absolutePanel(
-              id = "controls", class = "panel panel-default", fixed = TRUE,
+              id = "controls", class = "panel panel-default", fixed = FALSE,
               draggable = TRUE, top = 60, left = "auto", right = 8, bottom = "auto",
               width = 450, height = "auto",
             tableOutput("plot_clickedpoints"))
